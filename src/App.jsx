18a7fbd89b34381043535f2a88868cc792de1bd8
HimanshiSh03 <himanshi03dev@gmail.com>
2025-07-11 13:35:31 +0530

    Initial commit: Tic Tac Toe React Game

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Board from './Board';
+
+function App() {
+return (
+    <>
+      <h1><b><u>Tic Tac Toe</u></b></h1>
+      <Board/>
+    </>
+  );
+}
+
+export default App; 
---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Square=(props)=>{
+    return (
+        <div 
+        onClick={props.onClick}
+        style={{border:"1px solid brown", height: "100px", width: "100px", display:"flex", justifyContent: "center",  alignItems: 'center'}}
+        className='square'>
+            <h5>{props.value}</h5>
+        </div>
+    );
+};
+export default Square; 
